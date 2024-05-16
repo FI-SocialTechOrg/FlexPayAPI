04c0c7c8b24382596207197e6fa2e4702b6c8e3d
NotLT42 <u20211c667@upc.edu.pe>
2024-05-16 15:33:16 -0500

    Feature: Creation of the Role entity
    The Role entity was created
    
    Co-Authored-By: Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
    Co-Authored-By: Luis Mario Gonzales Anaya <u20201c585@upc.edu.pe>
    Co-Authored-By: Lucía A. <91093842+luciaaliagat@users.noreply.github.com>

--- src/main/java/com/flexpay/restapi/FlexPayAPI/domain/entities/Role.java
+++ src/main/java/com/flexpay/restapi/FlexPayAPI/domain/entities/Role.java
@@ -1,4 +1,25 @@
 package com.flexpay.restapi.FlexPayAPI.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "role")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "type", length = 15, nullable = false)
+    private String type;
 }
