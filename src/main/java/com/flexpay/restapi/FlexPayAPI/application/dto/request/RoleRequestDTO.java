3acb4f687693ef6b854f78343800e7adaccf6461
NotLT42 <u20211c667@upc.edu.pe>
2024-05-16 15:39:54 -0500

    Feature: Creating the Role requestDTO
    Role requestDTO created

---
+++ src/main/java/com/flexpay/restapi/FlexPayAPI/application/dto/request/RoleRequestDTO.java
@@ -0,0 +1,17 @@
+package com.flexpay.restapi.FlexPayAPI.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleRequestDTO {
+
+    @NotBlank(message = "type is mandatory")
+    private String type;
+}
