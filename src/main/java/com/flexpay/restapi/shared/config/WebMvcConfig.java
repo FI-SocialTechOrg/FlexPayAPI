3d30c7cd7bf5abcc8452f6055535f47e772a6326
NotLT42 <u20211c667@upc.edu.pe>
2024-06-24 16:21:21 -0500

    Fix CORS

--- src/main/java/com/flexpay/restapi/shared/config/WebMvcConfig.java
+++ src/main/java/com/flexpay/restapi/shared/config/WebMvcConfig.java
@@ -10,7 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000/"
+                        "http://localhost:3000/",
+                        "https://flexpay-site.netlify.app/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
